e8c8f1b63f2dbf0ec68b774bc82faf611f540f28
deepkolos <deepkolos@qq.com>
2025-07-25 10:27:59 +0800

    feat(types): 扩展手势事件类型以支持更多触控板操作
    添加 isScroll、isScale、isRotate、magnification 和 deltaAngle 字段到手势事件回调中，以便更全面地处理触控板的各种手势操作

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,16 @@
 declare module "@deepkolos/electron-trackpad-utils" {
 	export function onTrackpadScrollBegan(callback: () => void): void;
 	export function onTrackpadScrollEnded(callback: () => void): void;
-	export function onScroll(callback: (event: { deltaX: number, deltaY: number, isTrackpad: boolean }) => void): void;
+	export function onGesture(callback: (event: {
+		deltaX: number,
+		deltaY: number,
+		isTrackpad: boolean,
+		isScroll: boolean,
+		isScale: boolean,
+		isRotate: boolean,
+		magnification: number,
+		deltaAngle: number
+	}) => void): void;
 	export function onForceClick(callback: () => void): void;
 	export function triggerFeedback(): void;
 }
